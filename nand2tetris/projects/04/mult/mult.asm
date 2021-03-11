2b059b8648ba1054ffd968583e27ea3c07aa1a33
Kem Crimmins <kem.crimmins@gmail.com>
2021-03-11 18:55:58 -0500

    add mult.asm

--- nand2tetris/projects/04/mult/mult.asm
+++ nand2tetris/projects/04/mult/mult.asm
@@ -7,3 +7,42 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
 // Put your code here.
+
+// pseudo code
+// R2 = 0 initialize result
+// for (i=0; i<R1; i++) {  Add R0 to result R1 times
+//    R2 = R2 + R0
+//    i = i + 1
+//  }
+// end pseudo code
+
+@R2
+M=0  // set results register to 0
+@i
+M=0 // initialize i
+
+(LOOP)
+// if (i == R1) goto END
+@i
+D=M
+@R1
+D=D-M
+@END
+D; JEQ
+
+// R2 = R2 + R0
+@R0
+D=M
+@R2
+M=D+M
+
+// i++
+@i
+M=M+1
+
+@LOOP
+0; JMP
+
+(END)
+@END
+0; JMP
